75312844b93d71d5bde69dd21469e797bc2e3c6f
唐芳华 <fanghua.t@thelook.cn>
2018-04-15 10:06:01 +0800

    #38 重新搭配好的单 ，wms 这边状态没有更新

--- src/main/java/thelook/testmb4/DS1.java
+++ src/main/java/thelook/testmb4/DS1.java
@@ -1,28 +1,29 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package thelook.testmb4;
-//
-//import com.zaxxer.hikari.HikariDataSource;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.boot.jdbc.DataSourceBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//
-///**
-// *
-// * @author tangfanghua
-// */
-//@Configuration
-//public class DS1 {
-//
-//    @Bean
-//    @Primary
-//    @ConfigurationProperties("app.oms2.datasource")
-//    public HikariDataSource dataSource() {
-//        return DataSourceBuilder.create().type(HikariDataSource.class).build();
-//    }
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thelook.testmb4;
+
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ *
+ * @author tangfanghua
+ */
+@Configuration
+public class DS1 {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "app.datasource")
+    public HikariDataSource dataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+}
